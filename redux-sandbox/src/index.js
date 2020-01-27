17b7a0ea7d9403dae0e3ff2f49af369f545a6816
Loki <zpk.loki@gmail.com>
2020-01-27 18:09:12 +0300

    Redux store

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,5 +1,6 @@
-const reducer = (state = 0, action) => {
+import {createStore}from "redux";
 
+const reducer = (state = 0, action) => {
   switch (action.type) {
     case "INC":
       return state + 1;
@@ -9,7 +10,22 @@ const reducer = (state = 0, action) => {
   }
 };
 
-let state = reducer(undefined, {})
-state = reducer(state, { type: "INC" });
+const store = createStore(reducer);
 
-console.log(state);
+store.subscribe(()=>{
+  console.log(store.getState())
+})
+
+// console.log(store.getState());
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+// console.log(store.getState());
+
+
+
+
+// Вручную созданный код
+// let state = reducer(undefined, {})
+// state = reducer(state, { type: "INC" });
+
+// console.log(state);
